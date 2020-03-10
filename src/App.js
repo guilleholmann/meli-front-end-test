96af4cd0008238c6558b4630f98b80f2c890334d
Guillermo Holmann <guilleholmann@gmail.com>
2020-03-09 23:15:38 -0300

    adding styles to searchbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 function App() {
@@ -7,7 +6,12 @@ function App() {
     <div className="App">
       <header className="app__header">
         <div className="app__nav app__container">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima hic labore excepturi id quod porro eum soluta iusto ipsam sit sint, maxime corrupti dolores. Quos aliquid quas voluptate cupiditate esse!
+          <a className="app__logo" href="/" tabIndex="4"><span>Mercado Libre - Donde comprar y vender de todo</span></a>
+          <form className="nav__search-form" action="" method="GET" role="search">
+            <input type="text" className="nav__search-input" aria-label="Ingresá lo que quieras encontrar" name="as_word" placeholder="Nunca dejes de buscar" maxLength="120" autoFocus="" autoCapitalize="off" autoCorrect="off" spellCheck="false" autoComplete="off" tabIndex="2"/>
+              <button type="submit" className="nav__search-btn" tabIndex="3"><div role="img" aria-label="Buscar" className="nav__icon-search"></div>
+              </button>
+          </form>
         </div>
       </header>
     </div>
