912bc8a1f0ac6588b60a51f0e0fc3546c577b995
Guillermo Holmann <guilleholmann@gmail.com>
2020-03-09 23:40:51 -0300

    creating searchbar component, moving styles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import './App.scss';
+import SearchBar from './components/SearchBar/SearchBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="app__header">
-        <div className="app__nav app__container">
-          <a className="app__logo" href="/" tabIndex="4"><span>Mercado Libre - Donde comprar y vender de todo</span></a>
-          <form className="nav__search-form" action="" method="GET" role="search">
-            <input type="text" className="nav__search-input" aria-label="Ingresá lo que quieras encontrar" name="as_word" placeholder="Nunca dejes de buscar" maxLength="120" autoFocus="" autoCapitalize="off" autoCorrect="off" spellCheck="false" autoComplete="off" tabIndex="2"/>
-              <button type="submit" className="nav__search-btn" tabIndex="3"><div role="img" aria-label="Buscar" className="nav__icon-search"></div>
-              </button>
-          </form>
-        </div>
-      </header>
+      <SearchBar/>
     </div>
   );
 }
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './SearchBar.scss';
+
+function SearchBar() {
+  return (
+    <div className="App">
+      <header className="app__header">
+        <div className="app__nav app__container">
+          <a className="app__logo" href="/" tabIndex="4"><span>Mercado Libre - Donde comprar y vender de todo</span></a>
+          <form className="nav__search-form" action="" method="GET" role="search">
+            <input type="text" className="nav__search-input" aria-label="Ingresá lo que quieras encontrar" placeholder="Nunca dejes de buscar" maxLength="120" autoFocus="" autoCapitalize="off" autoCorrect="off" spellCheck="false" autoComplete="off" tabIndex="2"/>
+              <button type="submit" className="nav__search-btn" tabIndex="3"><div role="img" aria-label="Buscar" className="nav__icon-search"></div>
+              </button>
+          </form>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default SearchBar;
