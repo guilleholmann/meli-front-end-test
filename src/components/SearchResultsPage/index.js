14cbb473a485a0ced32a1766b42b7706003f51e9
Guillermo Holmann <guilleholmann@gmail.com>
2020-03-15 17:24:23 -0300

    styles for listing page

--- src/components/SearchResultsPage/index.js
+++ src/components/SearchResultsPage/index.js
@@ -25,20 +25,24 @@ const SearchResults = ({ location }) => {
   return (
     <React.Fragment>
       {!loading ? (
-        <div className="app__container section__listing-products">
-          { results.map(item => (
-              <div className="product__item" key={item.id}>
-                <Link to={`/items/${item.id}`}>
-                  <div>
-                    <img src={item.picture} alt="" />
-                    <span>{item.price.amount}</span>
-                    <h1> {item.id}</h1>
-                    <h3> {item.title}</h3>
+        <main className="app__container results__section">
+          <ol className="results__list">
+            {results.map(item => (
+              <li className="results__list-item" key={item.id}>
+                  <div class="item__image-container">
+                    <Link  to={`/items/${item.id}`}>
+                      <img className="item__image" src={item.picture} alt={item.title} />
+                    </Link>
                   </div>
-                </Link>
-              </div>
+                  <div className="item__info-container">
+                    <span>{item.price.amount}</span>
+                    <h1>{item.id}</h1>
+                    <h3>{item.title}</h3>
+                  </div>
+              </li>
             ))}
-        </div>
+          </ol>
+        </main>
       ) : (
         <Spinner />
       )}
