c20fe3b4e592ad31075c1e931e0b1a40d205e891
Guillermo Holmann <guilleholmann@gmail.com>
2020-03-15 18:03:00 -0300

    validating if we have results

--- src/components/SearchResultsPage/index.js
+++ src/components/SearchResultsPage/index.js
@@ -26,22 +26,38 @@ const SearchResults = ({ location }) => {
     <React.Fragment>
       {!loading ? (
         <main className="app__container results__section">
+          {results.length > 0 &&
           <ol className="results__list">
             {results.map(item => (
               <li className="results__list-item" key={item.id}>
-                  <div class="item__image-container">
+                  <div className="item__image-container">
                     <Link  to={`/items/${item.id}`}>
                       <img className="item__image" src={item.picture} alt={item.title} />
                     </Link>
                   </div>
                   <div className="item__info-container">
-                    <span>{item.price.amount}</span>
-                    <h1>{item.id}</h1>
-                    <h3>{item.title}</h3>
+                    <span className="item__price">$ {item.price.amount}</span>
+                    <h2>{item.title}</h2>
+                  </div>
+                  <div className="item__city-container">
+                    <span>{item.address}</span>
+            
                   </div>
               </li>
             ))}
           </ol>
+        }
+         {results.length === 0 &&
+          <div className="no-results-container">
+            <div className="help-data">
+              <span className="title">No hay publicaciones que coincidan con tu búsqueda.</span>
+              <ul className="tips-list">
+                <li>Revisá la ortografía de la palabra.</li>
+                <li>Utilizá palabras más genéricas o menos palabras.</li>
+              </ul>
+            </div>
+          </div>
+        }
         </main>
       ) : (
         <Spinner />
