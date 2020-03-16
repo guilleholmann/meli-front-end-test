1166ccdcc3065256fd59899d4bba5ab3d103f6ea
Guillermo Holmann <guilleholmann@gmail.com>
2020-03-15 23:02:59 -0300

    adding more styles

--- src/components/ProductDetails/index.js
+++ src/components/ProductDetails/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import Spinner from "../Spinner";
 import { showProductDetails } from "../../services/product-service";
 import "./styles.scss";
-import { CURRENCY_SYMBOL_MAPPER, CONDITION_MAPPER  } from "../../utils";
+import { CURRENCY_SYMBOL_MAPPER, CONDITION_MAPPER } from "../../utils";
 
 function ProductDetail({ match }) {
   const [product, setProduct] = useState({});
@@ -24,41 +24,43 @@ function ProductDetail({ match }) {
   return (
     <React.Fragment>
       {!loading && product.item ? (
-        <div className="app__container">
-          <div className="item__section">
-            <div className="item__column-container">
-              <div className="item__column-left">
-                <img
-                  className="item__picture"
-                  src={product.item.picture}
-                  alt={product.item.title}
-                />
-                <div className="item__description">
-                  <h2>Descripción del producto</h2>
-                  <p className="item__description-text">
-                    {product.item.description}
-                  </p>
-                </div>
+        <div className="item__section app__container">
+          <div className="item__column-container">
+            <div className="item__column-left">
+              <img
+                className="item__picture"
+                src={product.item.picture}
+                alt={product.item.title}
+              />
+              <div className="item__description">
+                <h2>Descripción del producto</h2>
+                <p className="item__description-text">
+                  {product.item.description}
+                </p>
               </div>
-              <div className="item__info item__column-right">
-                <div className="item__condition">
-                  <span className="conditions">
+            </div>
+            <div className="item__info item__column-right">
+              <div className="item__condition">
+                <span className="conditions">
                   {`
                     ${CONDITION_MAPPER[product.item.condition]}
-                    ${product.item.sold_quantity > 0 ? ' - ' + product.item.sold_quantity + ' vendidos' : ''}
+                    ${
+                      product.item.sold_quantity > 0
+                        ? " - " + product.item.sold_quantity + " vendidos"
+                        : ""
+                    }
                   `}
                 </span>
-                </div>
-                <div className="item__price">
-                  <span>
-                    {CURRENCY_SYMBOL_MAPPER[product.item.price.currency]}
-                  </span>
-                  {product.item.price.amount}
-                </div>
-                <h1 className="item__title">{product.item.title}</h1>
-                <div className="btn__container">
-                  <button className="btn__shop">COMPRAR</button>
-                </div>
+              </div>
+              <div className="item__price">
+                <span>
+                  {CURRENCY_SYMBOL_MAPPER[product.item.price.currency]}
+                </span>
+                {product.item.price.amount}
+              </div>
+              <h1 className="item__title">{product.item.title}</h1>
+              <div className="btn__container">
+                <button className="btn__shop">COMPRAR</button>
               </div>
             </div>
           </div>
